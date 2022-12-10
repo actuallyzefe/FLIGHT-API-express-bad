4fb561ea3d25aa2c559b9776e63cb3948dcef07e
actuallyzefe <zefek10@gmail.com>
2022-12-11 02:51:34 +0300

    reset Function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const authController = require("../controllers/authController");
-const userController = require("../controllers/userController");
+const express = require('express');
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 const router = express.Router();
 
-router.route("/").get(userController.getAllUsers);
-router.route("/signup").post(authController.signUp);
-router.route("/login").post(authController.login);
-router.route("/forgotPassword").post(authController.forgotPassword);
+router.route('/').get(userController.getAllUsers);
+router.route('/signup').post(authController.signUp);
+router.route('/login').post(authController.login);
+router.route('/forgotPassword').post(authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
 module.exports = router;
