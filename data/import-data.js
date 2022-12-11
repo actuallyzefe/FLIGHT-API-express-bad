9cc6dd5de0f137d025b41c238c0629be625458b5
actuallyzefe <zefek10@gmail.com>
2022-12-11 21:38:12 +0300

    data

--- data/import-data.js
+++ data/import-data.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
-const fs = require("fs");
-const Flight = require("../models/ticketModel");
-const User = require("../models/userModel");
-const { dirname } = require("path");
-const dotenv = require("dotenv");
-dotenv.config({ path: "./.env" });
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Flight = require('../models/ticketModel');
+const User = require('../models/userModel');
+const { dirname } = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -12,18 +12,17 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(console.log("Successfully connected to db"));
+  .then(console.log('Successfully connected to db'));
 
 // READ JSON FILE
-const flights = JSON.parse(fs.readFileSync(`./data/flight-data.json`, "utf-8"));
-const users = JSON.parse(fs.readFileSync(`./data/user-data.json`, "utf-8"));
+const flights = JSON.parse(fs.readFileSync(`./data/flight-data.json`, 'utf-8'));
+// const users = JSON.parse(fs.readFileSync(`./data/user-data.json`, 'utf-8'));
 
 // IMPORT TO DB
 const importData = async () => {
   try {
     await Flight.create(flights);
-    // await User.create(users);
-    console.log("DATA SUCCESSFULLY IMPORTED");
+    console.log('DATA SUCCESSFULLY IMPORTED');
   } catch (err) {
     console.log(err);
   }
@@ -34,17 +33,16 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Flight.deleteMany();
-    await User.deleteMany();
-    console.log("DATA SUCCESSFULLY DELETED");
+    console.log('DATA SUCCESSFULLY DELETED');
   } catch (err) {
     console.log(err);
   }
   process.exit();
 };
 
-if (process.argv[2] === "--import") {
+if (process.argv[2] === '--import') {
   importData();
-} else if (process.argv[2] === "--delete") {
+} else if (process.argv[2] === '--delete') {
   deleteData();
 }
 console.log(process.argv);
