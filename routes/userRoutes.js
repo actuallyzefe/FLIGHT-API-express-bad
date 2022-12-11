ab749f84e514314098d0c9b6e1bbe73bcb363a31
actuallyzefe <zefek10@gmail.com>
2022-12-11 18:11:37 +0300

    protected Routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,15 +8,12 @@ router.route('/').get(userController.getAllUsers);
 
 router.route('/signup').post(authController.signUp);
 router.route('/login').post(authController.login);
-router
-  .route('/forgotPassword')
-  .post(authController.protect, authController.forgotPassword);
-router
-  .route('/resetPassword/:resetToken')
-  .patch(authController.protect, authController.resetPassword);
 
-router
-  .route('/updatePassword')
-  .patch(authController.protect, authController.updatePassword);
+// PROTECTED ROUTES
+router.use(authController.protect);
+router.route('/forgotPassword').post(authController.forgotPassword);
+router.route('/resetPassword/:resetToken').patch(authController.resetPassword);
+
+router.route('/updatePassword').patch(authController.updatePassword);
 
 module.exports = router;
