8277f2112f4f46e4161e66a1124d196b30557c51
actuallyzefe <zefek10@gmail.com>
2022-12-11 22:51:48 +0300

    flight filter

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -3,13 +3,10 @@ const Flight = require('../models/ticketModel');
 exports.getAllFlights = async (req, res) => {
   try {
     const queryObj = { ...req.query };
-    // const eFields = ['page', 'sort', 'limit', 'fields'];
-    // eFields.forEach((el) => delete queryObj[el]);
-
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    const query = Flight.find(JSON.parse(queryStr));
 
+    const query = Flight.find(JSON.parse(queryStr));
     const tickets = await query; // Burada await yapmasam paginate vessaire ekleyemezdim
 
     res.status(200).json({
